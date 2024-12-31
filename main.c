b0345cc9bf5cad383e47306cef2ebd3d2845b233
ilovepersimmon <qingniu.career@gmail.com>
2024-12-30 22:56:03 -0800

    Add simple REPL and InputBuffer (#1)

---
+++ main.c
@@ -0,0 +1,75 @@
+//
+// Created by qniu on 12/16/24.
+//
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    // allocates memory
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() {
+    printf("db > ");
+}
+
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+      printf("Error reading input\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // printf("%d \n", bytes_read);
+    // printf("%s \n", input_buffer->buffer);
+
+    // ignore trailing new line
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+    // printf("%s \n", input_buffer->buffer[bytes_read - 1]);
+
+}
+
+// // lineptr: pointer to the var that we use to point to the buffer containing the read line
+// // n: pointer to the var we use to save the size of allocated buffer
+// // return the number of bytes we read
+// ssize_t getline(char** lineptr, size_t *n, FILE *stream) {
+//     // allocates memory
+// }
+
+void close_input_buffer(InputBuffer* input_buffer) {
+  free(input_buffer->buffer);
+  free(input_buffer);
+}
+
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+
+    // simple REPL: read-execute-print loop
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'. \n", input_buffer -> buffer);
+        }
+    }
+}
