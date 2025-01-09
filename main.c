de9c1d90654c5ffd6dfa72a4208f8ac837e60036
ilovepersimmon <qingniu.career@gmail.com>
2025-01-08 23:30:03 -0800

    Add select and insert basic parser (#2)
    * Add simple REPL and InputBuffer
    
    * Add select and insert basic parser
    
    ---------
    
    Co-authored-by: qniu <qniu@qnius-MacBook-Pro.local>

--- main.c
+++ main.c
@@ -7,6 +7,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED
+} MetaCommandResult;
+
+// note: unrecognized is a bit like an exception
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT,
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
 typedef struct {
     char* buffer;
     size_t buffer_length;
@@ -40,9 +60,9 @@ void read_input(InputBuffer* input_buffer) {
 
     // ignore trailing new line
     input_buffer->input_length = bytes_read - 1;
+    // make the last char pointing to 0
     input_buffer->buffer[bytes_read - 1] = 0;
     // printf("%s \n", input_buffer->buffer[bytes_read - 1]);
-
 }
 
 // // lineptr: pointer to the var that we use to point to the buffer containing the read line
@@ -57,19 +77,79 @@ void close_input_buffer(InputBuffer* input_buffer) {
   free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    // why do strncmp and strcmp here?
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case STATEMENT_INSERT:
+            printf("We would do an insert there. \n");
+            break;
+        case STATEMENT_SELECT:
+            printf("We would do a select there. \n");
+            break;
+    }
+}
+
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
 
     // simple REPL: read-execute-print loop
     while (true) {
+        // continue starts next loop
+        // 'break' breaks out of switch
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'. \n", input_buffer -> buffer);
+        if (input_buffer->buffer[0] == '.') {
+            // meta commands start with a dot
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED):
+                    printf("Unrecognized command: %s\n", input_buffer->buffer);
+                    continue;
+            }
         }
+
+        // converts the line of input into our internal representation of a statement
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                continue;
+        }
+
+        // eventually becomes our VM
+        execute_statement(&statement);
+        printf("Executed.\n");
+
+        // if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        //     close_input_buffer(input_buffer);
+        //     exit(EXIT_SUCCESS);
+        // } else {
+        //     printf("Unrecognized command '%s'. \n", input_buffer -> buffer);
+        // }
     }
 }
